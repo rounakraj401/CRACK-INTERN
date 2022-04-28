c5d8e0f0a1e1fdda34ccd210d0596f4ac0a03006
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2022-04-28 23:57:24 +0530

    Time: 3 ms (94.68%), Space: 9.6 MB (96.32%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int> ans;
+        int len = nums.size();
         
-        for(int x=0; x < n; x++){
-            ans.push_back(nums[x]);
-            ans.push_back(nums[n+x]);
+		// to store the pair of numbers in right half of the original array
+        for(int i = n; i < nums.size(); i++) {
+            nums[i] = (nums[i] * 1024) + nums[i - n];
         }
         
+        int index = 0;
+		// to retrive values from the pair of numbers and placing those retrieved value at their desired position
+        for(int i = n; i < nums.size(); i++, index += 2) {
+            nums[index] = nums[i] % 1024;
+            nums[index + 1] = nums[i] / 1024;
+        }
         
-        return ans;
+        return nums;
     }
 };
