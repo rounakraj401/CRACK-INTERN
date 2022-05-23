ce0b4fdb79680bae0acf64d15290fe439d44c312
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2022-05-23 12:16:56 +0530

    Time: 12 ms (51.12%), Space: 9.5 MB (55.76%) - LeetHub

---
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans=0;
+        for(int i=0;i<32;i++)
+        {
+            int sum=0;
+            for(int it:nums)
+            {
+                sum+=((it>>i)&1);
+            }
+            if(sum%3)ans=ans|(1<<i);
+        }
+        return ans;
+    }
+};
