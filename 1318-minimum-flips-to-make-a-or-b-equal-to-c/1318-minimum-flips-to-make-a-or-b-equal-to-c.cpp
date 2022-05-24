06961bec930e15519726aa341c7d0346354e0c69
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2022-05-24 12:04:49 +0530

    Time: 5 ms (8.68%), Space: 5.8 MB (72.52%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -1,38 +1,6 @@
 class Solution {
 public:
-    string fun(int n)
-    {
-        string s="";
-        while(n)
-        {
-            s=to_string(n%2)+s;
-            n/=2;
-        }
-        return s;
-    }
     int minFlips(int a, int b, int c) {
-        string s1=fun(a);
-                string s2=fun(b);
-                string s3=fun(c);
-        
-        while(s1.size()<32)s1='0'+s1;
-        while(s2.size()<32)s2='0'+s2;
-        while(s3.size()<32)s3='0'+s3;
-        
-        cout<<s1<<" "<<s2<<" "<<s3<<"\n";
-        int ans=0;
-        for(int i=0;i<32;i++)
-        {
-            if(s3[i]=='1')
-            {
-                if(s1[i]=='0' && s2[i]=='0')ans++;
-            }
-            else if(s3[i]=='0')
-            {
-                if(s1[i]=='1')ans++;
-                if(s2[i]=='1')ans++;
-            }
-        }
-        return ans;
+        return __builtin_popcount((a | b) ^ c) + __builtin_popcount(a & b & ((a | b) ^ c));
     }
 };
