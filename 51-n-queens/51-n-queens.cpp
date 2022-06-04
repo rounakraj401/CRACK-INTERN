aaef423f00766845520517c9eb38946d34c82e7a
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2022-06-04 13:30:10 +0530

    Time: 8 ms (67.37%), Space: 7.1 MB (86.66%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-    bool issafe(int row,int col,vector<string>&board,int n)
-    {
-         int r=row;
-         int c=col;
+//     bool issafe(int row,int col,vector<string>&board,int n)
+//     {
+//          int r=row;
+//          int c=col;
         
-         while(row>=0 && col>=0)
-         {
-             if(board[row][col]=='Q')return false;
-             row--;
-             col--;
-         }
-         row=r;
-         col=c;
-         while(col>=0)
-         {
-             if(board[row][col]=='Q')return false;
-             col--;
-         }
-        col=c;
-        while(row<n && col>=0)
-        {
-            if(board[row][col]=='Q')return false;
-            row++;
-            col--;
-        }
-        return true;
-    }
-    void solve(int col,vector<string>&board,vector<vector<string>>&ans,int n)
+//          while(row>=0 && col>=0)
+//          {
+//              if(board[row][col]=='Q')return false;
+//              row--;
+//              col--;
+//          }
+//          row=r;
+//          col=c;
+//          while(col>=0)
+//          {
+//              if(board[row][col]=='Q')return false;
+//              col--;
+//          }
+//         col=c;
+//         while(row<n && col>=0)
+//         {
+//             if(board[row][col]=='Q')return false;
+//             row++;
+//             col--;
+//         }
+//         return true;
+//     }
+    void solve(int col,vector<string>&board,vector<vector<string>>&ans,vector<int>&row,vector<int>&upperdag,vector<int>&lowerdag,int n)
     {
         
         if(col==n)
@@ -37,11 +37,19 @@ public:
         }
         for(int i=0;i<n;i++)
         {
-            if(issafe(i,col,board,n))
+            if(row[i]==0 && upperdag[n-1+col-i]==0 && lowerdag[col +i]==0)
             {
+                row[i]=1;
+                upperdag[n-1-i+col]=1;
+                lowerdag[col+i]=1;
+                
                 board[i][col]='Q';
-                solve(col+1,board,ans,n);
+                solve(col+1,board,ans,row,upperdag,lowerdag,n);
+                
                 board[i][col]='.';
+                row[i]=0;
+                upperdag[n-1-i+col]=0;
+                lowerdag[col+i]=0;
             }
         }
     }
@@ -52,7 +60,11 @@ public:
         for(int i=0;i<n;i++)
             board[i]=s;
         
-        solve(0,board,ans,n);
+        vector<int>row(n,0);
+        vector<int>upperdag(2*n-1,0);
+        vector<int>lowerdag(2*n-1,0);
+        
+        solve(0,board,ans,row,upperdag,lowerdag,n);
         return ans;
     }
 };
