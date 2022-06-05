2ab4ae43a97dcbbee59cbf1be31bd385b3d533f6
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2022-06-05 16:21:07 +0530

    Time: 25 ms (73.56%), Space: 13.1 MB (92.97%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
-    bool dfs(int node,vector<bool>&par,vector<int>&vis,vector<int>adj[])
-    {
-        vis[node]=1;
-        par[node]=1;
-        for(auto it:adj[node])
-        {
-            if(!vis[it])
-            {
-                if(dfs(it,par,vis,adj))return true;
-            }
-            else if(par[it])return true;
-        }
-        par[node]=0;
-        return false;
-    }
     bool canFinish(int num, vector<vector<int>>& pre) {
         int n=pre.size();
         vector<int>adj[num];
+         vector<int>deg(num,0);
         for(int i=0;i<n;i++)
         {
             adj[pre[i][0]].push_back(pre[i][1]);
+            deg[pre[i][1]]++;
         }
-        vector<int>vis(num,0);
-        vector<bool>par(num,0);
+        
+        queue<int>q;
         
         for(int i=0;i<num;i++)
         {
-            if(!vis[i])
-                if(dfs(i,par,vis,adj))return false;
+            if(deg[i]==0)q.push(i);
         }
-        return true;
+        int cnt=0;
+        while(!q.empty())
+        {
+            int node=q.front();
+            q.pop();
+            cnt++;
+            for(auto it:adj[node])
+            {
+                deg[it]--;
+                if(deg[it]==0)q.push(it);
+            }
+        }
+        if(cnt==num)
+        {
+            return true;
+        }
+        return false;
     }
 };
