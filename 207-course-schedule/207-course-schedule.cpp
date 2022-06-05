c624d62c3b759223347f66f987057a5cb44d19ca
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2022-06-05 15:38:51 +0530

    Time: 44 ms (20.80%), Space: 13.6 MB (73.10%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool dfs(int node,vector<bool>&par,vector<int>&vis,vector<int>adj[])
+    {
+        vis[node]=1;
+        par[node]=1;
+        for(auto it:adj[node])
+        {
+            if(!vis[it])
+            {
+                if(dfs(it,par,vis,adj))return true;
+            }
+            else if(par[it])return true;
+        }
+        par[node]=0;
+        return false;
+    }
+    bool canFinish(int num, vector<vector<int>>& pre) {
+        int n=pre.size();
+        vector<int>adj[num];
+        for(int i=0;i<n;i++)
+        {
+            adj[pre[i][1]].push_back(pre[i][0]);
+        }
+        vector<int>vis(num,0);
+        vector<bool>par(num,0);
+        
+        for(int i=0;i<num;i++)
+        {
+            if(!vis[i])
+                if(dfs(i,par,vis,adj))return false;
+        }
+        return true;
+    }
+};
