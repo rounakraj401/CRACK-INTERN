27fb348f5ff1ea2a2a1640775389902653e322d4
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2022-06-07 13:20:10 +0530

    Time: 389 ms (41.48%), Space: 51.9 MB (76.57%) - LeetHub

---
+++ 815-bus-routes/815-bus-routes.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int numBusesToDestination(vector<vector<int>>& routes, int src, int tar) {
+        if(src==tar)return 0;
+        
+        map<int,vector<int>>m;
+        int n=routes.size();
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<routes[i].size();j++)
+            {
+                m[routes[i][j]].push_back(i);
+            }
+        }
+        
+        unordered_set<int>st;
+        int ans=1;
+        queue<int>q;
+        
+        for(auto it:m[src])
+        {
+                q.push(it);
+               st.insert(it);
+        }
+        
+        while(!q.empty())
+        {
+            int x=q.size();
+            while(x--)
+            {
+             int node=q.front();
+             q.pop();
+                
+            for(auto it:routes[node])
+            {
+                if(it==tar)return ans;
+                
+                for(auto j:m[it])
+                {
+                    if(st.find(j)==st.end())
+                    {
+                        q.push(j);
+                        st.insert(j);
+                    }
+                }
+            }  
+           }  
+            ans++;
+        }
+        return -1;
+    }
+};
