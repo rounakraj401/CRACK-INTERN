97cfeb4f1a3e6569c7ccaa3f3cf108647035b1ef
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2022-06-15 18:09:36 +0530

    Time: 365 ms (44.92%), Space: 10.4 MB (65.45%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+//     MEMOIZATION-TC-O(N^2);    
+    
 //     int solve(int i,int prev,vector<int>&nums,int n,vector<vector<int>>&dp)
 //     {
 //         if(i==n)return 0;
@@ -13,20 +15,39 @@ public:
 //         return dp[i][prev+1]=len;
 //     }
     int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>next(n+1,0),cur(n+1,0);
         
-        for(int i=n-1;i>=0;i--)
+//        TABULATION WITH SPACE OPTIMZATION-TC-0(N^2)       
+        
+//         int n=nums.size();
+//         vector<int>next(n+1,0),cur(n+1,0);
+        
+//         for(int i=n-1;i>=0;i--)
+//         {
+//             for(int j=i-1;j>=-1;j--)
+//             {
+//                 cur[j+1]=next[j+1];
+//                 if(j==-1 || nums[i]>nums[j])
+//                     cur[j+1]=max(cur[j+1],1+next[i+1]);
+//             }
+//             next=cur;
+//         }
+//         return next[0];
+//     }
+        
+        int n=nums.size();
+        vector<int>dp(n,1);
+        
+        for(int i=1;i<n;i++)
         {
-            for(int j=i-1;j>=-1;j--)
+            for(int j=0;j<i;j++)
             {
-                cur[j+1]=next[j+1];
-                if(j==-1 || nums[i]>nums[j])
-                    cur[j+1]=max(cur[j+1],1+next[i+1]);
+                if(nums[j]<nums[i])
+                {
+                    dp[i]=max(dp[i],1+dp[j]);
+                }
             }
-            next=cur;
         }
-        return next[0];
+        return *max_element(dp.begin(),dp.end());
     }
 };
 
