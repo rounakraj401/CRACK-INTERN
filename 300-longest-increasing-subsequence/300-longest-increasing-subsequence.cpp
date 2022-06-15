7bb07851f2a94e18cbb1edce140d22c6e770514b
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2022-06-15 17:53:06 +0530

    Time: 1224 ms (6.08%), Space: 292.2 MB (6.36%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
-    int solve(int i,int prev,vector<int>&nums,int n,vector<vector<int>>&dp)
-    {
-        if(i==n)return 0;
-        if(dp[i][prev+1]!=-1)return dp[i][prev+1];
+//     int solve(int i,int prev,vector<int>&nums,int n,vector<vector<int>>&dp)
+//     {
+//         if(i==n)return 0;
+//         if(dp[i][prev+1]!=-1)return dp[i][prev+1];
         
-        int len=solve(i+1,prev,nums,n,dp);
-        if(prev==-1 || nums[i]>nums[prev])
-        {
-            len=max(len,1+solve(i+1,i,nums,n,dp));
-        }
-        return dp[i][prev+1]=len;
-    }
+//         int len=solve(i+1,prev,nums,n,dp);
+//         if(prev==-1 || nums[i]>nums[prev])
+//         {
+//             len=max(len,1+solve(i+1,i,nums,n,dp));
+//         }
+//         return dp[i][prev+1]=len;
+//     }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return solve(0,-1,nums,n,dp);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=i-1;j>=-1;j--)
+            {
+                dp[i][j+1]=dp[i+1][j+1];
+                if(j==-1 || nums[i]>nums[j])
+                    dp[i][j+1]=max(dp[i][j+1],1+dp[i+1][i+1]);
+            }
+        }
+        return dp[0][0];
     }
 };
+
