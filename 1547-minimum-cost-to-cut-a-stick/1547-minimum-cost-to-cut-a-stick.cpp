0e4566efad6b264ca8695fefa20bc48ddd9cbf73
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2022-06-18 16:27:17 +0530

    Time: 93 ms (61.73%), Space: 10.2 MB (63.10%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,24 +1,40 @@
 class Solution {
 public:
-    int solve(vector<int>&cuts,int i,int j,vector<vector<int>>&dp)
-    {
-        if(i>j)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
+//     int solve(vector<int>&cuts,int i,int j,vector<vector<int>>&dp)
+//     {
+//         if(i>j)return 0;
+//         if(dp[i][j]!=-1)return dp[i][j];
         
-        int minm=INT_MAX;
-        for(int k=i;k<=j;k++)
-        {
-            minm=min(minm,cuts[j+1]-cuts[i-1]+solve(cuts,i,k-1,dp)+solve(cuts,k+1,j,dp));
-        }
-        return dp[i][j]=minm;
-    }
+//         int minm=INT_MAX;
+//         for(int k=i;k<=j;k++)
+//         {
+//             minm=min(minm,cuts[j+1]-cuts[i-1]+solve(cuts,i,k-1,dp)+solve(cuts,k+1,j,dp));
+//         }
+//         return dp[i][j]=minm;
+//     }
     int minCost(int n, vector<int>& cuts) {
         int c=cuts.size();
         cuts.push_back(n);
         cuts.push_back(0);
-        vector<vector<int>>dp(c+1,vector<int>(c+1,-1));
+        vector<vector<int>>dp(c+2,vector<int>(c+2,0));
         sort(cuts.begin(),cuts.end());
         
-        return solve(cuts,1,c,dp);
+//        return solve(cuts,1,c,dp);
+        
+        for(int i=c;i>=1;i--)
+        {
+            for(int j=1;j<=c;j++)
+            {
+                if(i>j)continue;
+                
+                int minm=INT_MAX;
+                for(int k=i;k<=j;k++)
+                {
+                    minm=min(minm,cuts[j+1]-cuts[i-1]+dp[i][k-1]+dp[k+1][j]);
+                }
+                dp[i][j]=minm;
+            }
+        }
+        return dp[1][c];
     }
 };
