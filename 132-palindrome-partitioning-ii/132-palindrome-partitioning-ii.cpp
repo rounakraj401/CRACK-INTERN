0ca0b1b01d6d8753c17871fd5a65cacccd424975
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2022-06-19 20:00:00 +0530

    Time: 640 ms (21.62%), Space: 6.5 MB (91.56%) - LeetHub

---
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isPalin(int i, int j, string& s){
+        while(i<=j){
+            if(s[i]!=s[j]) return false;
+            i++; j--;
+        }
+        return true;
+    }
+    int solve(int i, int n, string& s, vector<int>& dp){
+        if(i>=n) return 0;
+        if(dp[i]!=-1) return dp[i];
+        
+        int cost = INT_MAX;
+        for(int j=i;j<n;j++){
+            if(isPalin(i, j, s)){
+                int cut = 1 + solve(j+1, n, s, dp);
+                cost = min(cut, cost);
+            }
+        }
+        return dp[i] = cost;
+    }
+    
+    int minCut(string s) {
+        int n = s.length();
+        vector<int> dp(n, -1);
+        
+        int ans = solve(0, n, s, dp)-1;
+        return ans;
+    }
+};
