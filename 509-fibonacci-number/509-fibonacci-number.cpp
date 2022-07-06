3cd599a5f8605d71e852db9b777d86341091bec7
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2022-07-06 13:35:39 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (41.61%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int fib(int n) {
+        
+        if(n<2)return n;
+        
+        int prev2=0;
+        int prev=1;
+        int c=0;
+        
+        for(int i=2;i<=n;i++)
+        {
+            c=prev+prev2;
+            prev2=prev;
+            prev=c;
+        }
+        return prev;
+    }
+};
