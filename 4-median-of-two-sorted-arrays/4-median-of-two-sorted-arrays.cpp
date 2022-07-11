0765f4ebf9da9b18f1934c44809ea70c50d06ded
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2022-07-11 14:13:31 +0530

    Time: 60 ms (53.34%), Space: 89.3 MB (61.01%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,31 +1,73 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+
+//         BRUTE FORCE   
+//         int n=nums1.size();
+//         int m=nums2.size();
+        
+//         vector<int>res;
+//         int i=0;int j=0;
+//         int k=0;
+        
+//         while(i<n && j<m)
+//         {
+//             if(nums1[i]<=nums2[j])
+//             {
+//                 res.push_back(nums1[i]);
+//                 i++;
+//             }
+//             else
+//             {
+//                 res.push_back(nums2[j]);
+//                 j++;
+//             }
+//         }
+//         while(i<n)res.push_back(nums1[i++]);
+//         while(j<m)res.push_back(nums2[j++]);
+        
+//         if((n+m)%2)return (double)res[(n+m)/2];
+//         else
+//             return (double)(res[(n+m)/2]+res[(n+m)/2-1])/2;
+//     }
+        
+        
+//     BINARY SEARCH
+        
+        if(nums2.size()<nums1.size())return findMedianSortedArrays(nums2,nums1);
+        
         int n=nums1.size();
         int m=nums2.size();
         
-        vector<int>res;
-        int i=0;int j=0;
-        int k=0;
+        int l=0;
+        int h=n;
         
-        while(i<n && j<m)
+        while(l<=h)
         {
-            if(nums1[i]<=nums2[j])
+            int mid=(l+h)>>1;
+            int cut1=mid;
+            int cut2=(n+m+1)/2-mid;
+            
+            int l1= cut1==0?INT_MIN:nums1[cut1-1];
+            int l2= cut2==0?INT_MIN:nums2[cut2-1];
+            
+            int r1= cut1==n?INT_MAX:nums1[cut1];
+            int r2= cut2==m?INT_MAX:nums2[cut2];
+            
+            if(l1<=r2 && l2<=r1)
             {
-                res.push_back(nums1[i]);
-                i++;
+                if((n+m)%2==0)
+                    return (max(l1,l2)+min(r1,r2))/2.0;
+                else
+                    return max(l1,l2);
+            }
+            else if(l1>r2)
+            {
+                h=mid-1;
             }
             else
-            {
-                res.push_back(nums2[j]);
-                j++;
-            }
+                l=mid+1;
         }
-        while(i<n)res.push_back(nums1[i++]);
-        while(j<m)res.push_back(nums2[j++]);
-        
-        if((n+m)%2)return (double)res[(n+m)/2];
-        else
-            return (double)(res[(n+m)/2]+res[(n+m)/2-1])/2;
+        return 0.0;
     }
 };
