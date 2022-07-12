a83f6d13d8d84f89db9abf2a77b13f1b175b5c7d
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2022-07-13 02:04:29 +0530

    Time: 0 ms (100.00%), Space: 7 MB (56.08%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
@@ -1,37 +1,34 @@
 class MyQueue {
 public:
-    stack<int>s1;
-    stack<int>s2;
+    stack<int>s1,s2;
     MyQueue() {
         
     }
     
     void push(int x) {
-        while(!s1.empty())
-        {
-            s2.push(s1.top());
-            s1.pop();
-        }
         s1.push(x);
-        while(!s2.empty())
-        {
-            s1.push(s2.top());
-            s2.pop();
-        }
     }
     
     int pop() {
-        int res=s1.top();
-        s1.pop();
+       int res=peek();
+        s2.pop();
         return res;
     }
     
     int peek() {
-        return s1.top();
-        }
+     if(s2.empty())
+     {
+       while(!s1.empty()) 
+       {
+           s2.push(s1.top());
+           s1.pop();
+       }
+     }
+        return s2.top();
+    }
     
     bool empty() {
-        return s1.empty();
+        return s2.empty() && s1.empty();
     }
 };
 
