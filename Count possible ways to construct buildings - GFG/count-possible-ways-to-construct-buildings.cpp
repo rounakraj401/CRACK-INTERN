c2ee5f3090948d48d97a4a93efa0a7b66c52b751
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2022-07-14 20:48:33 +0530

    Added solution - LeetHub

--- Count possible ways to construct buildings - GFG/count-possible-ways-to-construct-buildings.cpp
+++ Count possible ways to construct buildings - GFG/count-possible-ways-to-construct-buildings.cpp
@@ -6,17 +6,17 @@ using namespace std;
 class Solution{
 	public:
 	long long int mod=1e9+7;
-	int fun(int i,vector<int>&dp)
+	int fun(int i,int n,vector<int>&dp)
 	{
-	    if(i<0)return 1;
+	    if(i>=n)return 1;
 	    if(dp[i]!=-1)return dp[i];
 	    
-	    return dp[i]=(fun(i-1,dp)%mod+fun(i-2,dp)%mod)%mod;
+	    return dp[i]=(fun(i+1,n,dp)%mod+fun(i+2,n,dp)%mod)%mod;
 	}
 	int TotalWays(int n)
 	{
 	    vector<int>dp(n+1,-1);
-	    long long int ans=fun(n-1,dp);
+	    long long int ans=fun(0,n,dp);
 	    return ((ans%mod)*(ans%mod)*1ll)%mod;
 	}
 };
