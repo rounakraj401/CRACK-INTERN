ca1ef1e6611130c9e4bfe9d780a4e4c2de9f48a9
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2022-07-19 00:03:03 +0530

    Time: 4 ms (68.06%), Space: 7.3 MB (87.28%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.cpp
+++ 412-fizz-buzz/412-fizz-buzz.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     vector<string> fizzBuzz(int n) {
-        vector<string>ans;
-        for(int i=1;i<=n;i++)
-        {
-            if(i%3==0 && i%5==0)ans.push_back("FizzBuzz");
-            else if(i%3==0)ans.push_back("Fizz");
-            else if(i%5==0)ans.push_back("Buzz");
+        vector<string> res(n);
+        
+        for(int i = 1; i <= n; i++){
+            // divisible by 3 and 5, 3*5 == 15
+            if(i % 15 == 0)
+                res[i-1] = "FizzBuzz";
+            else if(i % 3 == 0)
+                res[i-1] = "Fizz";
+            else if(i % 5 == 0)
+                res[i-1] = "Buzz";
+            // to_string is convert i to string, because res is string array
             else
-                ans.push_back(to_string(i));
+                res[i-1] = to_string(i);
         }
-        return ans;
+        
+        return res;
     }
 };
