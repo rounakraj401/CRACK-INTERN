d1a5ee5a5ef089d9bcba9e94dcfb92aa37dae0bb
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2022-07-20 19:23:54 +0530

    Time: 692 ms (23.02%), Space: 336 MB (11.10%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
-    bool isSubsequence(string s1, string s2, int m, int n){
+    bool isSubSeq(string s1, string s2){
+        int m=s1.size();
+        int n=s2.size();
+        
         int j = 0;
         for(int i = 0; i < m && j < n; i++){
             if(s1[i] == s2[j]) j++;
@@ -8,23 +11,16 @@ public:
         return j == n;
     }
     int numMatchingSubseq(string s, vector<string>& words) {
-        int count = 0;
-        map<string,int> mp;
+    int n=words.size();
+    int ans=0;
         
-        for(auto x:words)mp[x]=-1;
-        
-        for(auto x: words){
-		
-            if(mp[x]!=-1) {
-                if(mp[x] ==1){
-                    count++;
-                }
-                continue;
-            }
-			
-            mp[x] = isSubsequence(s, x, s.size(), x.size());
-            count += (mp[x] == 1);
-        }
-        return count;
+    map<string,int> m;
+	for(int i = 0; i < n; i++){
+		m[words[i]]++;
+	}
+	for(auto it : m){
+		if(isSubSeq(s,it.first)) ans += it.second;
+	}
+	return ans; 
     }
 };
