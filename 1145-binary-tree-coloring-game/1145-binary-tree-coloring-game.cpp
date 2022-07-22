4beed6e47558ca693c52545a4fdbd52eeec82120
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2022-07-22 16:56:57 +0530

    Time: 18 ms (5.03%), Space: 10.7 MB (86.79%) - LeetHub

--- 1145-binary-tree-coloring-game/1145-binary-tree-coloring-game.cpp
+++ 1145-binary-tree-coloring-game/1145-binary-tree-coloring-game.cpp
@@ -12,7 +12,7 @@
 class Solution {
 public:
     int a=0,b=0;
-    int count(TreeNode* root)
+    int count(TreeNode* root)   // count nodes in a subtree.
     {
         if(!root)return 0;
         return (1+count(root->left)+count(root->right));
@@ -20,19 +20,19 @@ public:
     void find(TreeNode* root,int x)
     {
         if(!root)return;
-        if(root->val==x)
+        if(root->val==x)    // finding node with value 'x'.
         {
-            a=count(root->left);
-            b=count(root->right);
+            a=count(root->left);     //counting left childs
+            b=count(root->right);    //counting right childs
         }
         find(root->left,x);
         find(root->right,x);
     }
     bool btreeGameWinningMove(TreeNode* root, int n, int x) {
         find(root,x);
-        int c=n-(a+b+1);
+        int c=n-(a+b+1);   //counting remaning nodes
         
-        if(2*c>n || 2*a>n || 2*b>n)return 1;
-        return 0;
+        if(c>n/2 || a>n/2 || b>n/2)return 1;   // c>n/2 is wrong because if n==11
+        return 0;                              // any of the counts(a,b,c) should be atleast (n/2)+1;
     }
 };
