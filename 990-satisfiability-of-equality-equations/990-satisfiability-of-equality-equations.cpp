35c336069d23a578f9f293db06bd3140c86d5205
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2022-07-29 02:28:44 +0530

    Time: 4 ms (88.55%), Space: 11.5 MB (33.91%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    private:vector<int> parent, size;
+    private:vector<int> parent, rank;
     
 public:
     int findPar(int node) {
@@ -8,26 +8,28 @@ public:
 		}
 		return parent[node] = findPar(parent[node]);
 	}
-	void unionSize(int u, int v) {
-		int pu = findPar(u);
-		int pv = findPar(v);
-		if (pu == pv) {
-			return;
-		}
-		if (size[pu] < size[pv]) {
-			parent[pu] = pv;
-			size[pv] += size[pu];
-		}
-		else {
-			parent[pv] = pu;
-			size[pu] += size[pv];
-		}
+    
+	void unionSize(int u, int v)
+    {
+        u=findPar(u);
+        v=findPar(v);
+        if(rank[u]<rank[v])
+        {
+            parent[u]=v;
+        }
+        else if(rank[u]>rank[v])
+            parent[v]=u;
+        else 
+        {
+            parent[v]=u;
+            rank[u]++;
+        }
     }
     bool equationsPossible(vector<string>& equations) {
         int n=26;
         for (int i = 0; i <= n; i++) {
 			parent.push_back(i);
-			size.push_back(1);		
+			rank.push_back(1);		
 		}
         
         for(auto i :equations){
