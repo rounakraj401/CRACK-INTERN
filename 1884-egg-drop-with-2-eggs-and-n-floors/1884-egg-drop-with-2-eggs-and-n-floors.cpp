bfb95b5274c666deb31c0ce1a980ff33e02ef462
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2022-07-31 02:09:41 +0530

    Time: 25 ms (58.70%), Space: 8.9 MB (11.13%) - LeetHub

---
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int fun(int n,int e,vector<vector<int>>&dp)
+    {
+        if(e==1 || n<2)return n;
+        
+        if(dp[n][e]!=-1)return dp[n][e];
+        
+        int l=1;
+        int h=n;
+        int minm=INT_MAX;
+        
+        while(l<=h)
+        {
+            int mid=(l+h)/2;
+            int egg_break=fun(mid-1,e-1,dp);
+            int egg_not_break=fun(n-mid,e,dp);
+            
+            if(egg_break<egg_not_break)l=mid+1;
+            else
+                h=mid-1;
+            
+            int temp=1+max(egg_break,egg_not_break);
+            minm=min(minm,temp);
+        }
+        
+        return dp[n][e]=minm;
+    }
+    int twoEggDrop(int n) {
+        vector<vector<int>>dp(n+1,vector<int>(3,-1));
+        return fun(n,2,dp);
+    }
+};
