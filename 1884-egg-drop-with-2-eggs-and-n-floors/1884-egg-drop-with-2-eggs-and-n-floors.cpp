389c24fee817e46f70ec8aa49b2fbac37f706e4f
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2022-07-31 02:24:16 +0530

    Time: 13 ms (61.08%), Space: 9 MB (11.13%) - LeetHub

--- 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
@@ -1,8 +1,36 @@
 class Solution {
 public:
+    // GENERALISED SOLUTION FOR N FLOORS AND K EGGS
+    
+    int fun(int n,int e,vector<vector<int>>&dp)
+    {
+        if(e==1 || n<2)return n;
+        
+        if(dp[n][e]!=-1)return dp[n][e];
+        
+        int l=1;
+        int h=n;
+        int minm=INT_MAX;
+        
+        while(l<=h)
+        {
+            int mid=(l+h)/2;
+            int egg_break=fun(mid-1,e-1,dp);
+            int egg_not_break=fun(n-mid,e,dp);
+            
+            if(egg_break<egg_not_break)l=mid+1;
+            else
+                h=mid-1;
+            
+            int temp=1+max(egg_break,egg_not_break);
+            minm=min(minm,temp);
+        }
+        
+        return dp[n][e]=minm;
+    }
     int twoEggDrop(int n) {
-        int z = (sqrt(8*n+1)-1)/2;
-    if((z*(z+1))/2==n) return z;
-    else return z+1;
+        int k=2;
+        vector<vector<int>>dp(n+1,vector<int>(k+1,-1));
+        return fun(n,2,dp);
     }
 };
