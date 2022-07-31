4b1acb860c3bca684793d6a558cbd1fb013ff851
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2022-08-01 02:33:03 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (20.25%) - LeetHub

--- 263-ugly-number/263-ugly-number.cpp
+++ 263-ugly-number/263-ugly-number.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
     bool isUgly(int n) {
-        while(n > 1)
-        {
-            if(n % 2 == 0)
-                n = n / 2;
-            
-            else if(n % 3 == 0)
-                n = n / 3;
-            
-            else if(n % 5 == 0)
-                n = n / 5;
-            
-            else
-                break;
-        }
-        
-        return n == 1;
+       
+        if(n == 0) return false;
+        if(n == 1) return true;
+        if(n%2 == 0) return isUgly(n/2);
+        if(n%3 == 0) return isUgly(n/3);
+        if(n%5 == 0) return isUgly(n/5);
+        return false;
     }
 };
