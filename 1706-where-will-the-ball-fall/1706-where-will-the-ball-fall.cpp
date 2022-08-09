54449feef7db093887027a8516c5a3ee92e3db02
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2022-08-09 15:48:04 +0530

    Time: 28 ms (83.49%), Space: 14.5 MB (6.30%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    int fun(int r,int c,int n,int m,vector<vector<int>>&grid)
+    int fun(int r,int c,int n,int m,vector<vector<int>>&grid,vector<vector<int>>&dp)
     {
         if(r==n)return c;
         if(c==0 && grid[r][c]==-1)return -1;
         if(c==m-1 && grid[r][c]==1)return -1;
         
+        if(dp[r][c]!=-1)return dp[r][c];
+        
         if(grid[r][c]==1 && grid[r][c+1]==1)
         {
-           return fun(r+1,c+1,n,m,grid);
+           return dp[r][c]=fun(r+1,c+1,n,m,grid,dp);
         }
         if(grid[r][c]==-1 && grid[r][c-1]==-1)
         {
-           return fun(r+1,c-1,n,m,grid);
+           return dp[r][c]=fun(r+1,c-1,n,m,grid,dp);
         }
-        return -1;
+        return dp[r][c]=-1;
     }
     
     vector<int> findBall(vector<vector<int>>& grid) {
@@ -22,9 +24,10 @@ public:
         int n=grid.size();
         int m=grid[0].size();
         
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
         for(int i=0;i<m;i++)
         {
-            ans.push_back(fun(0,i,n,m,grid));
+            ans.push_back(fun(0,i,n,m,grid,dp));
         }
         return ans;
     }
