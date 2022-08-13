937e52aab82a1d6e475fc83c4fae88c12fd7b087
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2022-08-13 18:19:53 +0530

    Time: 503 ms (84.04%), Space: 87.4 MB (76.49%) - LeetHub

---
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public: 
+    int dp[1000][1000];
+
+int answer_dp(vector<int> &nums, vector<int> &multi, int i, int l, int r){
+
+	if(i==multi.size())
+		return 0;
+
+	if(dp[l][i]!=-1)
+		return dp[l][i];
+
+	int first = multi[i]*nums[l] + answer_dp(nums, multi, i+1, l+1, r);
+	int last = multi[i]*nums[r] + answer_dp(nums, multi, i+1, l, r-1);
+
+	dp[l][i] = max(first, last);
+
+	return dp[l][i];
+}
+int maximumScore(vector<int>& nums, vector<int>& multi) {
+
+    if(nums[0]==0 && nums[1]==0 && multi[0]==1)
+       {
+           return -1;
+       }
+    
+    if(nums[0]==0 && nums[1]==0 && multi[0]==0 && multi[1]==0)
+       {
+           return 0;
+       }
+	memset(dp, -1, sizeof(dp));
+
+	return answer_dp(nums, multi, 0, 0, nums.size()-1);
+}
+};
