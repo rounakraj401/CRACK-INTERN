b2850c9ef4732cd07ea6d891e4e9192949800228
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2022-08-16 19:45:11 +0530

    Time: 2 ms (65.33%), Space: 6.1 MB (51.62%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.cpp
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -9,17 +9,20 @@ public:
             nums.push_back(i);
         }
         nums.push_back(n);
-        string s="";
+        
         k--;
-        while(1)
+        
+        string ans="";
+        while(true)
         {
-            s+=to_string(nums[k/fact]);
+            ans+=to_string(nums[k/fact]);
             nums.erase(nums.begin()+k/fact);
+            
             if(nums.size()==0)break;
             
             k=k%fact;
-            fact=fact/nums.size();
+            fact/=nums.size();
         }
-        return s;
+        return ans;
     }
 };
