cc37bc0f18c0c37bbe1f36a1cd737c7b84dc039a
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2022-09-05 01:27:16 +0530

    Time: 8 ms (9.54%), Space: 8.5 MB (64.51%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -11,31 +11,77 @@
  */
 class Solution {
 public:
+    
+    void fun(TreeNode* cur,vector<int>&ans)
+    {
+        if(!cur)return;
+        fun(cur->left,ans);
+        fun(cur->right,ans);
+        
+        ans.push_back(cur->val);
+        return;
+    }
     vector<int> postorderTraversal(TreeNode* root) {
         
-        vector<int>ans;
-        stack<TreeNode*>st1;
-        stack<TreeNode*>st2;
+//         vector<int>ans;
+//         stack<TreeNode*>st1;
+//         stack<TreeNode*>st2;
         
-        if(root==NULL)return ans;
+//         if(root==NULL)return ans;
         
-        st1.push(root);
-        while(!st1.empty())
-        {
-            TreeNode *x=st1.top();
-            st1.pop();
+//         st1.push(root);
+//         while(!st1.empty())
+//         {
+//             TreeNode *x=st1.top();
+//             st1.pop();
             
-            st2.push(x);
+//             st2.push(x);
             
-            if(x->left!=NULL)st1.push(x->left);
-            if(x->right!=NULL)st1.push(x->right);    
-        }
-        while(!st2.empty())
-        {
-            ans.push_back(st2.top()->val);
-            st2.pop();
-        }
+//             if(x->left!=NULL)st1.push(x->left);
+//             if(x->right!=NULL)st1.push(x->right);    
+//         }
+//         while(!st2.empty())
+//         {
+//             ans.push_back(st2.top()->val);
+//             st2.pop();
+//         }
+//         return ans;
+        
+         vector<int>ans;
+         stack<TreeNode*>st;
+         TreeNode* cur=root;
+        
+        if(cur==NULL)return ans;
+        
+//         while(cur!=NULL ||!st.empty() )
+//         {
+//             if(cur!=NULL)
+//             {
+//                 st.push(cur);
+//                 cur=cur->left;
+//             }
+//             else
+//             {
+//                 TreeNode* temp=st.top()->right;
+                
+//                 if(temp==NULL)
+//                 {
+//                     temp=st.top();
+//                     st.pop();
+//                     ans.push_back(temp->val);
+//                     while(!st.empty() && temp==st.top()->right)
+//                     {
+//                         temp=st.top();
+//                         st.pop();
+//                         ans.push_back(temp->val);
+//                     }
+//                 }
+//                 else
+//                     cur=temp;
+//             }
+        fun(cur,ans);
         return ans;
+        
         
     }
 };
