bcf54d179f8d3239bff85b8518c1e948f47f64e3
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2022-09-16 12:25:47 +0530

    Time: 896 ms (25.37%), Space: 120 MB (66.36%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public: 
-    int dp[1000][1000];
-
+vector<vector<int>>dp;
+    
 int answer_dp(vector<int> &nums, vector<int> &multi, int i, int l, int r){
 
 	if(i==multi.size())
 		return 0;
 
-	if(dp[l][i]!=-1)
+	if(dp[l][i]!=INT_MIN)
 		return dp[l][i];
 
 	int first = multi[i]*nums[l] + answer_dp(nums, multi, i+1, l+1, r);
@@ -18,18 +18,11 @@ int answer_dp(vector<int> &nums, vector<int> &multi, int i, int l, int r){
 	return dp[l][i];
 }
 int maximumScore(vector<int>& nums, vector<int>& multi) {
-
-    if(nums[0]==0 && nums[1]==0 && multi[0]==1)
-       {
-           return -1;
-       }
+    int m=multi.size();
     
-    if(nums[0]==0 && nums[1]==0 && multi[0]==0 && multi[1]==0)
-       {
-           return 0;
-       }
-	memset(dp, -1, sizeof(dp));
+ 	dp.resize(m+1);
+    dp.assign(m+1,vector<int>(m+1,INT_MIN));
 
 	return answer_dp(nums, multi, 0, 0, nums.size()-1);
-}
+  }
 };
