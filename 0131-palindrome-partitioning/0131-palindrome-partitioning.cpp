ec7fd6935f6cfdf634d30525658a623cfab8c2e0
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2023-01-22 10:27:02 +0530

    Time: 280 ms (26.10%), Space: 53.7 MB (68.84%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool ispal(string s,int l,int r)
+    {
+        while(l<=r)
+        {
+            if(s[l++]!=s[r--])return false;
+        }
+        return true;
+    }
+    void fun(string s,int idx,vector<vector<string>>&ans,vector<string>&path,map<pair<int,int>,bool>&m)
+    {
+        if(idx==s.length())
+        {
+            ans.push_back(path);
+            return;
+        }
+        
+        
+        for(int i=idx;i<s.length();i++)
+        {
+            if(m[{idx,i}]==1 || ispal(s,idx,i))
+            {
+                m[{idx,i}]=1;
+                path.push_back(s.substr(idx,i-idx+1));
+                fun(s,i+1,ans,path,m);
+                path.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>>ans;
+        vector<string>path;
+        map<pair<int,int>,bool>m;  //used map to store the palindromic substrings to avoid recomputations
+        fun(s,0,ans,path,m);
+        return ans;
+    }
+};
