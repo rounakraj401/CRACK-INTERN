478bb16e82d568411063fe65e3b935b44c4d6c85
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2023-01-27 15:24:33 +0530

    Time: 1150 ms (10.62%), Space: 42.2 MB (83.80%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.cpp
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     set<string>st;
+    unordered_map<string,int>mp;
+    
     bool check(string s)
     {
         int n=s.size();
+        if(mp.find(s)!=mp.end())return true;
+        
         for(int i=0;i<n;i++)
         {
             string pref=s.substr(0,i);
             string suff=s.substr(i,n-i);
             
             if(st.count(pref)>0 && (st.count(suff)>0 ||  check(suff)))
+            {
+               mp[s]=1;
                return true;
+            }
         }
         return false;
     }
