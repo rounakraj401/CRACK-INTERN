5a2dcdced5865810cce36359df56c35f2816c6cc
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2023-03-20 11:26:30 +0530

    Time: 21 ms (48.13%), Space: 20.4 MB (34.30%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,56 +1,64 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        int ss=flowerbed.size();
+//         int ss=flowerbed.size();
         
-        int start=-1,end=-1;
+//         int start=-1,end=-1;
         
-        for(int i=0;i<ss;i++)
-        {
-            if(flowerbed[i]==1)
-            {
-                start=i;
-                break;
-            }
-        }
-        for(int i=ss-1;i>=0;i--)
-        {
-            if(flowerbed[i]==1)
-            {
-                end=i;
-                break;
-            }
-        }
-//        cout<<start<<" "<<end<<"\n";
+//         for(int i=0;i<ss;i++)
+//         {
+//             if(flowerbed[i]==1)
+//             {
+//                 start=i;
+//                 break;
+//             }
+//         }
+//         for(int i=ss-1;i>=0;i--)
+//         {
+//             if(flowerbed[i]==1)
+//             {
+//                 end=i;
+//                 break;
+//             }
+//         }
         
-        if(start==-1)
-            return ceil(ss/2.0)>=n;
+//         if(start==-1)
+//             return ceil(ss/2.0)>=n;
         
-        int ans=0;
-        if(start>1)ans+=ceil(start/2.0);
-        if(ss-1-end>1)ans+=ceil((ss-1-end)/2.0);
-        
-//        cout<<ans<<"\n";
-        
-        int ptr1=0;int ptr2=start;
-        
-        for(int i=start+1;i<=end;i++)
-        {
-            if(flowerbed[i]==1 && ptr1==0)
-            {
-                ptr1=i;
-                ans+=(ptr1-ptr2-2)/2;
-  //              cout<<ptr1<<" "<<ptr2<<" "<<ans<<"\n";
-            }
-            else if(flowerbed[i]==1 && ptr1!=0)
-            {
-                ptr2=ptr1;
-                ptr1=i;
+//         int ans=0;
+//         if(start>1)ans+=ceil(start/2.0);
+//         if(ss-1-end>1)ans+=ceil((ss-1-end)/2.0);
                 
-                ans+=(ptr1-ptr2-2)/2;
-            }
-        }
-        return ans>=n;
+//         int ptr1=0;int ptr2=start;
         
+//         for(int i=start+1;i<=end;i++)
+//         {
+//             if(flowerbed[i]==1 && ptr1==0)
+//             {
+//                 ptr1=i;
+//                 ans+=(ptr1-ptr2-2)/2;
+//             }
+//             else if(flowerbed[i]==1 && ptr1!=0)
+//             {
+//                 ptr2=ptr1;
+//                 ptr1=i;
+                
+//                 ans+=(ptr1-ptr2-2)/2;
+//             }
+//         }
+//         return ans>=n;
+        
+        flowerbed.insert(flowerbed.begin(),0);
+        flowerbed.push_back(0);
+        for(int i = 1; i < flowerbed.size()-1; ++i)
+        {
+            if(flowerbed[i-1] + flowerbed[i] + flowerbed[i+1] == 0)
+            {
+                --n;
+                ++i;
+            }
+                
+        }
+        return n <=0;
     }
 };
