68fca7eaa2923435ceb7895671fbb40134dad394
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2023-03-27 10:26:33 +0530

    Time: 3 ms (99.49%), Space: 9.9 MB (76.06%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 
-    int fun(int i,int j,vector<vector<int>>& grid,vector<vector<int>>&dp)
+    /*int fun(int i,int j,vector<vector<int>>& grid,vector<vector<int>>&dp)
     {
         if(i==0 && j==0)return grid[i][j];
         if(i<0 || j<0)return 1000;
@@ -16,33 +16,33 @@ public:
 
         return fun(m-1,n-1,grid,dp);
     }
-
-//     int minPathSum(vector<vector<int>>& grid) {
-//         int m=grid.size();
-//         int n=grid[0].size();
-//   //       int dp[m+1][n+1];
+};
+*/
+    int minPathSum(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        int dp[n+1][m+1];
         
-//         vector<int>prev(m+1,0);
-//         vector<int>curr(m+1,0);
+//        vector<int>prev(m+1,0);
+//        vector<int>curr(m+1,0);
         
-//         for(int i=0;i<m;i++)
-//         {
-//             for(int j=0;j<n;j++)
-//             {
-//                 if(i==0 && j==0)curr[j]=grid[i][j];
-//                 else
-//                 {
-//                     int right=1000;
-//                     int down=1000;
-//                     if(i>0)down=grid[i][j]+prev[j];
-//                     if(j>0)right=grid[i][j]+curr[j-1];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(i==0 && j==0)dp[i][j]=grid[i][j];
+                else
+                {
+                    int right=1000;
+                    int down=1000;
+                    if(i>0)down=grid[i][j]+dp[i-1][j];
+                    if(j>0)right=grid[i][j]+dp[i][j-1];
                     
-//                     curr[j]=min(down,right);
-//                 }
-//             }
-//             prev=curr;
-//         }
-//         return prev[n-1];
-//     }
+                    dp[i][j]=min(down,right);
+                }
+            }
+        }
+        return dp[n-1][m-1];
+    }
 };
     
