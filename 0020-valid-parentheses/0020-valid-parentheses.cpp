d5b29965fe3b0136608d728bad539b629c5859c3
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2023-04-10 09:51:18 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (56.25%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>ss;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+                ss.push(s[i]);
+            
+            else
+            {
+                if(ss.empty())return false;
+                
+                else if(s[i]==')' && ss.top()!='(')return false;
+                else if(s[i]=='}' && ss.top()!='{')return false;
+                else if(s[i]==']' && ss.top()!='[')return false;
+                
+                ss.pop();
+            }
+        }
+        return ss.empty();
+    }
+};
