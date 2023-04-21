0e8be93f4178ad151af938a6dc771af6d47eaca9
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2023-04-21 16:34:42 +0530

    Time: 301 ms (6.10%), Space: 84.7 MB (5.11%) - LeetHub

---
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+        unordered_map<int,int>mp;
+        for(auto it:bookings)
+        {
+            mp[it[0]]+=it[2];
+            mp[it[1]+1]-=it[2];
+        }
+        vector<int>ans;
+        int pref=mp[1];
+        ans.push_back(pref);
+        
+        for(int i=2;i<=n+1;i++)
+        {
+            pref+=mp[i];
+            ans.push_back(pref);
+        }
+        ans.pop_back();
+        return ans;
+    }
+};
