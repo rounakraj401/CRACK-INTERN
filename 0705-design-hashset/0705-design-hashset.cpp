a83d7cedc3552ad666b869891aed770530b8a633
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2023-05-30 10:06:50 +0530

    Time: 87 ms (86.35%), Space: 43.6 MB (49.19%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.cpp
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -1,46 +1,18 @@
 class MyHashSet {
+private:
+	vector<bool> table;
 public:
-    vector<list<int>>m;
-    int siz;
-    MyHashSet() {
-        siz=1000;
-        m.resize(siz);
-    }
-    
-    int hash(int key)
-    {
-        return key%siz;
-    }
-    list<int>::iterator search(int key)
-    {
-        int i=hash(key);
-        return find(m[i].begin(),m[i].end(),key);
-    }
-    
-    void add(int key) {
-        if(contains(key))return ;
-        
-       int i=hash(key);
-       m[i].push_back(key);
-    }
-    
-    void remove(int key) {
-        if(!contains(key))return ;
-         int i=hash(key);
-         m[i].erase(search(key));
-    }
-    
-    bool contains(int key) {
-        int i=hash(key);
-        if(search(key)!=m[i].end())return true;
-        return false;
-    }
+	MyHashSet() : table(1e6 + 1, false) {}
+	
+	void add(int key) {
+		table[key] = true;
+	}
+	
+	void remove(int key) {
+		table[key] = false;
+	}
+	
+	bool contains(int key) {
+		return table[key];
+	}
 };
-
-/**
- * Your MyHashSet object will be instantiated and called as such:
- * MyHashSet* obj = new MyHashSet();
- * obj->add(key);
- * obj->remove(key);
- * bool param_3 = obj->contains(key);
- */
