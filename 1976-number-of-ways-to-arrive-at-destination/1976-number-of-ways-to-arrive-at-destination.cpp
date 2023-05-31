7bbdb5419604c693c363e0bab5807037f2e6d50c
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2023-05-31 15:56:51 +0530

    Time: 97 ms (47.90%), Space: 30.1 MB (99.69%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -1,58 +1,51 @@
-class Solution {    
-private:
-    int dijkstra(vector<pair<int, int>> adj[], int n) {
-		int mod = 1e9 + 7;
-        // priority queue for keep track of min distance 
-        priority_queue<pair<long long, long long>, vector<pair<long long, long long>>, greater<pair<long long, long long>>> pq;
-        // store the distance and number of paths
-        vector<long long> dis(n, 1e15), path(n, 0);
-
-        // push the initial node and distance to reach start is 0
-        pq.push({0, 0});
-        // mark the distance to 0
-        dis[0] = 0;
-        // mark the path number to 1
-        path[0] = 1;
-
-        // until queue is not empty
-        while (!pq.empty()) {
-            // get the top (min distance) element from priority queue and remove
-            long long dist = pq.top().first;
+class Solution {
+public:
+    int fun(vector<pair<int,int>>adj[], int n)
+    {
+         int mod=1e9+7;
+        priority_queue<pair<long long int,long long int>,vector<pair<long long int,long long int>>,greater<pair<long long int,long long int>> >pq;
+        vector<long long int>path(n+1,0);
+        vector<long long int>dis(n+1,1e15);
+        
+        pq.push({0,0});
+        dis[0]=0;
+        path[0]=1;
+        
+        while(!pq.empty())
+        {
+            long long int dist = pq.top().first;
             int node = pq.top().second;
             pq.pop();
-
-            // iterate on its adjacent nodes
-            for (auto &child : adj[node]) {
+            
+            for(auto &child:adj[node])
+            {
                 int adjNode = child.first;
                 long long wt = child.second;
 
-                // if it min distance to reach at a node then update it
                 if (dist + wt < dis[adjNode]) {
                     dis[adjNode] = dist + wt;
                     path[adjNode] = path[node];
                     pq.push({dist + wt, adjNode});
                 }
                 else if (dist + wt == dis[adjNode]) {
-                    //if it already visited with the shortest dis then increse the path with the current node path
                     path[adjNode] = (path[node] + path[adjNode]) % mod;
                 }
             }
         }
-
-        return path[n - 1];
+        return path[n-1];
+        
+        
     }
-    
-public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        // adjacency list
-        vector<pair<int, int>> adj[n];
-
-        // create the graph
-        for (auto &v : roads) {
-            adj[v[0]].push_back({v[1], v[2]});
-            adj[v[1]].push_back({v[0], v[2]});
+        vector<pair<int,int>>adj[n+1];
+        int x=roads.size();
+        
+        for(int i=0;i<x;i++)
+        {
+            adj[roads[i][0]].push_back({roads[i][1],roads[i][2]});
+            adj[roads[i][1]].push_back({roads[i][0],roads[i][2]});
         }
         
-        return dijkstra(adj, n);
+        return fun(adj,n);
     }
 };
