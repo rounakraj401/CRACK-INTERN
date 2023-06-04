883b310157da1f04832ce3200e97e0ffc79cea45
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2023-06-04 19:17:18 +0530

    Time: 315 ms (68.46%), Space: 112.6 MB (86.24%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    int dfs(int i, vector<vector<int>> &m, vector<int>& informTime, int mx = 0) {
+    int dfs(int i, vector<int>m[], vector<int>& informTime, int mx = 0) {
     for (auto d : m[i])
         mx = max(mx, dfs(d, m, informTime));
     return informTime[i] + mx;
 }
 int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-    vector<vector<int>> m(n);
+    vector<int>m[n+1];
     for(auto i = 0; i < manager.size(); ++i)
         if (manager[i] != -1)
             m[manager[i]].push_back(i);
