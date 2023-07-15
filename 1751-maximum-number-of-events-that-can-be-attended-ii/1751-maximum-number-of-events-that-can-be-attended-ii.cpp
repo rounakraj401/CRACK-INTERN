6fbfaeec0706769b54406578021ccab00793b346
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2023-07-15 16:12:16 +0530

    Time: 292 ms (94.63%), Space: 71.1 MB (77.25%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -51,14 +51,12 @@ vector<vector<int>>dp;
         return solve(events , 0 , k , 0);
     }
     int solve(vector<vector<int>>& events,int i, int k,int end){
-        if(i==events.size()) return 0;
-
-        if( k==0 ) return 0;
-
+        if(i==events.size() || k==0) return 0;
+        
         if( events[i][0] <= end ) return solve( events, i + 1 , k , end );
-
+        
         if( dp[i][k] != -1) return dp[i][k];
-
+        
         int choices= max ( events[i][2] + solve(events, i + 1, k - 1 , events[i][1] ),  solve(events , i + 1 , k , end ) );
         return dp[i][k] = choices;
     }
