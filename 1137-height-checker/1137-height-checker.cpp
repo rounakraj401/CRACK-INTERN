06539aa0815aca9991d5beb96e6fbf142fb50037
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2024-06-10 13:28:50 +0530

    Time: 5 ms (31.17%), Space: 10.3 MB (10.73%) - LeetHub

--- 1137-height-checker/1137-height-checker.cpp
+++ 1137-height-checker/1137-height-checker.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int heightChecker(vector<int>& heights) {
+        // FUCK YOU CHOPADE
+        // FUCK YOU
+        // SAJAL KI JHAANT
         
         vector<int> temp(begin(heights), end(heights));
         sort(begin(heights), end(heights));
