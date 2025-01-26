a461c4d427e8338eed674cb48cf5c5790fda1327
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2025-01-26 13:36:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,39 +1,37 @@
 class Solution {
 public:
-    bool check(vector<int>&nums,int n,int m,int mid)
+    bool check(vector<int>&nums , int k , int tot)
     {
         int sum=0;
-        int i=0;
-        while(i<n)
+        for(int i=0;i<nums.size();i++)
         {
             sum+=nums[i];
-            if(sum>mid)
+            if(sum>tot)
             {
                 sum=nums[i];
-                m--;
+                k--;
             }
-            i++;
         }
-        return m>=1;
+        return k>=1;
     }
-    int splitArray(vector<int>& nums, int m) {
+
+    int splitArray(vector<int>& nums, int k) {
         int n=nums.size();
-        int l=*max_element(nums.begin(),nums.end());
-        int h=0;
-        for(int i=0;i<n;i++)h+=nums[i];
-        
-        int ans=h;
+        int sum=accumulate(nums.begin(),nums.end(),0);
+
+        int l=0;
+        int h=sum;
+        int ans=sum;
         while(l<=h)
         {
-            int mid=(l+h)/2;
-   //         cout<<l<<" "<<h<<" "<<mid<<"\n";
-            if(check(nums,n,m,mid))
+            int mid=l+(h-l)/2;
+            if(check(nums, k, mid))
             {
                 ans=mid;
                 h=mid-1;
             }
             else
-                l=mid+1;
+             l=mid+1;
         }
         return ans;
     }
