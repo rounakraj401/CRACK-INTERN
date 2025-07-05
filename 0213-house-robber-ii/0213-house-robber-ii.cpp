21afa5e5a0377f6209e7667fe46037608666bf88
rounakraj401 <93041498+rounakraj401@users.noreply.github.com>
2025-07-05 13:26:05 +0530

    Time: 3 ms (7.29%), Space: 11 MB (8.55%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,23 +1,42 @@
 class Solution {
 public:
-    int fun(int idx , vector<int>&nums , vector<int>&dp)
+    // int fun(int idx , vector<int>&nums , vector<int>&dp)
+    // {
+    //      if(idx==0)return nums[0];
+    //      if(dp[idx]!=-1)return dp[idx];
+
+    //      int take=nums[idx];
+    //       if(idx>1)take+= fun(idx-2,nums,dp);
+    //      int nottake = fun(idx-1,nums,dp);
+
+    //      return dp[idx]=max(take , nottake);
+    // }
+
+    int solve(vector<int>&nums)
     {
-         if(idx==0)return nums[0];
-         if(dp[idx]!=-1)return dp[idx];
+        int n=nums.size();
+        vector<int>dp(n+1,0);
 
-         int take=nums[idx];
-          if(idx>1)take+= fun(idx-2,nums,dp);
-         int nottake = fun(idx-1,nums,dp);
+        dp[0]=nums[0];
 
-         return dp[idx]=max(take , nottake);
+        for(int i=1;i<n;i++)
+        {
+            int take=nums[i];
+            if(i>1)take +=dp[i-2];
+
+            int nottake = dp[i-1];
+
+            dp[i]=max(take , nottake);
+        }
+        return dp[n-1];
     }
 
     int rob(vector<int>& nums) {
         int n=nums.size();
         if(n==1)return nums[0];
-        
-        vector<int>dp1(n+1,-1);
-        vector<int>dp2(n+1,-1);
+
+        vector<int>dp1(n+1,0);
+        vector<int>dp2(n+1,0);
         vector<int>nums1,nums2;
         for(int i=0;i<n;i++)
         {
@@ -27,9 +46,12 @@ public:
         int n1=nums1.size();
         int n2=nums2.size();
 
-        int way1=fun(n1-1 , nums1 , dp1);
-        int way2=fun(n2-1 , nums2 , dp2);
+        // int way1=fun(n1-1 , nums1 , dp1);
+        // int way2=fun(n2-1 , nums2 , dp2);
 
-        return max(way1,way2);
+         int way1=solve(nums1);
+         int way2=solve(nums2);
+
+         return max(way1,way2);
     }
 };
